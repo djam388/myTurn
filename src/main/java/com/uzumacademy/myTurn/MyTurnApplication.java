38f7ef9b110263e6ef76fa1b909c9c387fd5bf39
djam388 <djam388@hotmail.com>
2024-07-16 18:31:17 +0500

    Initialize

---
+++ src/main/java/com/uzumacademy/myTurn/MyTurnApplication.java
@@ -0,0 +1,13 @@
+package com.uzumacademy.myTurn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzumacademy/myTurn/MyTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzumacademy.myTurn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
