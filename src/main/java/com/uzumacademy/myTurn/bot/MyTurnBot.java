7d26897e738bd9380d720d3195439a5d4cae6fbc
dadabaej <djam388@hotmail.com>
2024-07-17 23:36:37 +0500

    Telegram Bot: inline keyboards

--- src/main/java/com/uzumacademy/myTurn/bot/MyTurnBot.java
+++ src/main/java/com/uzumacademy/myTurn/bot/MyTurnBot.java
@@ -14,7 +14,10 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -62,34 +65,58 @@ public class MyTurnBot extends TelegramLongPollingBot {
 
             if (update.getMessage().hasText()) {
                 String messageText = update.getMessage().getText();
-                logger.info("Received message from chat ID: {}", chatId);
-
-                if ("/start".equals(messageText)) {
-                    startRegistration(user);
-                } else if ("/doctors".equals(messageText)) {
-                    if (user.getRegistrationState() == User.RegistrationState.COMPLETED) {
-                        sendDoctorsList(chatId);
-                    } else {
-                        sendMessage(chatId, "Пожалуйста, завершите регистрацию для просмотра списка врачей.");
-                    }
-                } else if (messageText.startsWith("/book")) {
-                    if (user.getRegistrationState() == User.RegistrationState.COMPLETED) {
-                        handleBookingRequest(user, messageText);
-                    } else {
-                        sendMessage(chatId, "Пожалуйста, завершите регистрацию для записи к врачу.");
-                    }
-                } else if ("/myappointments".equals(messageText)) {
-                    if (user.getRegistrationState() == User.RegistrationState.COMPLETED) {
-                        sendUserAppointments(user);
-                    } else {
-                        sendMessage(chatId, "Пожалуйста, завершите регистрацию для просмотра ваших записей.");
-                    }
-                } else {
-                    processUserInput(user, messageText);
-                }
+                handleIncomingMessage(user, messageText);
             } else if (update.getMessage().hasContact()) {
                 processPhoneNumber(user, update.getMessage().getContact().getPhoneNumber());
             }
+        } else if (update.hasCallbackQuery()) {
+            String callData = update.getCallbackQuery().getData();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+            handleCallbackQuery(chatId, callData);
+        }
+    }
+
+    private void handleIncomingMessage(User user, String messageText) {
+        if ("/start".equals(messageText)) {
+            startRegistration(user);
+        } else if ("/menu".equals(messageText)) {
+            sendMainMenu(user.getChatId());
+        } else {
+            processUserInput(user, messageText);
+        }
+    }
+
+    private void handleCallbackQuery(long chatId, String callData) {
+        switch (callData) {
+            case "SHOW_DOCTORS":
+                sendDoctorsList(chatId);
+                break;
+            case "MY_APPOINTMENTS":
+                sendUserAppointments(userService.getUserByChatId(chatId));
+                break;
+            case "MY_PROFILE":
+                sendUserProfile(chatId);
+                break;
+            default:
+                if (callData.startsWith("DOCTOR_")) {
+                    Long doctorId = Long.parseLong(callData.split("_")[1]);
+                    sendDoctorDetails(chatId, doctorId);
+                } else if (callData.startsWith("BOOK_")) {
+                    Long doctorId = Long.parseLong(callData.split("_")[1]);
+                    handleBookingRequest(userService.getUserByChatId(chatId), doctorId);
+                }
+                break;
+        }
+    }
+
+    private void sendUserProfile(long chatId) {
+        User user = userService.getUserByChatId(chatId);
+        if (user != null) {
+            String profileInfo = String.format("Ваш профиль:\nИмя: %s\nФамилия: %s\nТелефон: %s",
+                    user.getFirstName(), user.getLastName(), user.getPhoneNumber());
+            sendMessage(chatId, profileInfo);
+        } else {
+            sendMessage(chatId, "Ошибка: пользователь не найден.");
         }
     }
 
@@ -114,32 +141,23 @@ public class MyTurnBot extends TelegramLongPollingBot {
                 requestPhoneNumber(user.getChatId());
                 break;
             case AWAITING_PHONE_NUMBER:
-                sendMessage(user.getChatId(), "Пожалуйста, используйте кнопку 'Отправить номер телефона' для предоставления вашего номера.");
+                try {
+                    authService.setPhoneNumber(user, messageText);
+                    completeRegistration(user);
+                } catch (IllegalArgumentException e) {
+                    sendMessage(user.getChatId(), "Неверный формат номера телефона. Пожалуйста, используйте кнопку 'Отправить номер телефона' или введите номер в формате +XXXXXXXXXXX.");
+                }
                 break;
             case COMPLETED:
-                sendMessage(user.getChatId(), "Вы уже зарегистрированы. Используйте команды /doctors, /book или /myappointments.");
+                sendMessage(user.getChatId(), "Используйте команду /menu для доступа к функциям бота.");
                 break;
-        }
-    }
-
-    private void processPhoneNumber(User user, String phoneNumber) {
-        if (user.getRegistrationState() == User.RegistrationState.AWAITING_PHONE_NUMBER) {
-            try {
-                authService.setPhoneNumber(user, phoneNumber);
-                user.setRegistrationState(User.RegistrationState.COMPLETED);
-                userService.updateUser(user);
-                sendMessage(user.getChatId(), "Регистрация завершена! Теперь вы можете использовать сервис записи к врачу.");
-            } catch (IllegalArgumentException e) {
-                sendMessage(user.getChatId(), "Неверный формат номера телефона. Пожалуйста, попробуйте еще раз.");
-                requestPhoneNumber(user.getChatId());
-            }
         }
     }
 
     private void requestPhoneNumber(long chatId) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
-        message.setText("Пожалуйста, предоставьте ваш номер телефона, нажав на кнопку ниже.");
+        message.setText("Пожалуйста, предоставьте ваш номер телефона, нажав на кнопку ниже или введите его вручную в формате +XXXXXXXXXXX.");
 
         ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
         List<KeyboardRow> keyboard = new ArrayList<>();
@@ -160,43 +178,127 @@ public class MyTurnBot extends TelegramLongPollingBot {
         }
     }
 
-    private void sendDoctorsList(long chatId) {
-        List<Doctor> doctors = doctorService.getAllDoctors();
-        StringBuilder message = new StringBuilder("Список доступных врачей:\n\n");
-        for (Doctor doctor : doctors) {
-            message.append(doctor.getFirstName()).append(" ")
-                    .append(doctor.getLastName()).append(" - ")
-                    .append(doctor.getSpecialization()).append("\n")
-                    .append("Для записи используйте команду: /book ")
-                    .append(doctor.getId()).append("\n\n");
+    private void processPhoneNumber(User user, String phoneNumber) {
+        if (user.getRegistrationState() == User.RegistrationState.AWAITING_PHONE_NUMBER) {
+            try {
+                authService.setPhoneNumber(user, phoneNumber);
+                completeRegistration(user);
+            } catch (IllegalArgumentException e) {
+                sendMessage(user.getChatId(), "Неверный формат номера телефона. Пожалуйста, попробуйте еще раз.");
+                requestPhoneNumber(user.getChatId());
+            }
         }
-        sendMessage(chatId, message.toString());
     }
 
-    private void handleBookingRequest(User user, String messageText) {
-        String[] parts = messageText.split(" ");
-        if (parts.length != 2) {
-            sendMessage(user.getChatId(), "Неверный формат команды. Используйте /book <id врача>");
+    private void completeRegistration(User user) {
+        user.setRegistrationState(User.RegistrationState.COMPLETED);
+        userService.updateUser(user);
+        sendMessageWithKeyboardRemove(user.getChatId(), "Регистрация завершена!");
+        sendMainMenu(user.getChatId());
+    }
+
+    private void sendMainMenu(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выберите действие:");
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        row1.add(InlineKeyboardButton.builder().text("Список врачей").callbackData("SHOW_DOCTORS").build());
+
+        List<InlineKeyboardButton> row2 = new ArrayList<>();
+        row2.add(InlineKeyboardButton.builder().text("Мои записи").callbackData("MY_APPOINTMENTS").build());
+
+        List<InlineKeyboardButton> row3 = new ArrayList<>();
+        row3.add(InlineKeyboardButton.builder().text("Мой профиль").callbackData("MY_PROFILE").build());
+
+        rowsInline.add(row1);
+        rowsInline.add(row2);
+        rowsInline.add(row3);
+
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            logger.error("Error sending main menu", e);
+        }
+    }
+
+    private void sendDoctorsList(long chatId) {
+        List<Doctor> doctors = doctorService.getAllDoctors();
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выберите врача:");
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+
+        for (Doctor doctor : doctors) {
+            List<InlineKeyboardButton> rowInline = new ArrayList<>();
+            rowInline.add(InlineKeyboardButton.builder()
+                    .text(doctor.getFirstName() + " " + doctor.getLastName() + " (" + doctor.getSpecialization() + ")")
+                    .callbackData("DOCTOR_" + doctor.getId())
+                    .build());
+            rowsInline.add(rowInline);
+        }
+
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            logger.error("Error sending doctors list", e);
+        }
+    }
+
+    private void sendDoctorDetails(long chatId, Long doctorId) {
+        Doctor doctor = doctorService.getDoctorById(doctorId);
+        if (doctor != null) {
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText("Врач: " + doctor.getFirstName() + " " + doctor.getLastName() + "\n" +
+                    "Специализация: " + doctor.getSpecialization() + "\n" +
+                    "Телефон: " + doctor.getPhoneNumber() + "\n" +
+                    "Email: " + doctor.getEmail());
+
+            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+            List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+
+            List<InlineKeyboardButton> rowInline = new ArrayList<>();
+            rowInline.add(InlineKeyboardButton.builder().text("Записаться на прием").callbackData("BOOK_" + doctorId).build());
+            rowsInline.add(rowInline);
+
+            markupInline.setKeyboard(rowsInline);
+            message.setReplyMarkup(markupInline);
+
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                logger.error("Error sending doctor details", e);
+            }
+        } else {
+            sendMessage(chatId, "Врач не найден.");
+        }
+    }
+
+    private void handleBookingRequest(User user, Long doctorId) {
+        Doctor doctor = doctorService.getDoctorById(doctorId);
+        if (doctor == null) {
+            sendMessage(user.getChatId(), "Врач с указанным ID не найден.");
             return;
         }
 
-        try {
-            Long doctorId = Long.parseLong(parts[1]);
-            Doctor doctor = doctorService.getDoctorById(doctorId);
-            if (doctor == null) {
-                sendMessage(user.getChatId(), "Врач с указанным ID не найден.");
-                return;
-            }
+        // Здесь должна быть логика выбора времени приема
+        LocalDateTime appointmentTime = LocalDateTime.now().plusDays(1).withHour(10).withMinute(0).withSecond(0).withNano(0);
 
-            // Здесь должна быть логика выбора времени приема
-            LocalDateTime appointmentTime = LocalDateTime.now().plusDays(1).withHour(10).withMinute(0).withSecond(0).withNano(0);
-
-            Appointment appointment = appointmentService.scheduleAppointment(user, doctor, appointmentTime);
-            sendMessage(user.getChatId(), "Вы успешно записаны к врачу " + doctor.getFirstName() + " " +
-                    doctor.getLastName() + " на " + appointmentTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
-        } catch (NumberFormatException e) {
-            sendMessage(user.getChatId(), "Неверный формат ID врача.");
-        }
+        Appointment appointment = appointmentService.scheduleAppointment(user, doctor, appointmentTime);
+        sendMessage(user.getChatId(), "Вы успешно записаны к врачу " + doctor.getFirstName() + " " +
+                doctor.getLastName() + " на " + appointmentTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
     }
 
     private void sendUserAppointments(User user) {
@@ -225,6 +327,23 @@ public class MyTurnBot extends TelegramLongPollingBot {
             logger.debug("Message sent to chat ID: {}", chatId);
         } catch (TelegramApiException e) {
             logger.error("Failed to send message to chat ID: {}", chatId, e);
+        }
+    }
+
+    private void sendMessageWithKeyboardRemove(long chatId, String text) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(text);
+
+        ReplyKeyboardRemove keyboardRemove = new ReplyKeyboardRemove();
+        keyboardRemove.setRemoveKeyboard(true);
+        message.setReplyMarkup(keyboardRemove);
+
+        try {
+            execute(message);
+            logger.debug("Message sent with keyboard remove to chat ID: {}", chatId);
+        } catch (TelegramApiException e) {
+            logger.error("Failed to send message with keyboard remove to chat ID: {}", chatId, e);
         }
     }
 }
